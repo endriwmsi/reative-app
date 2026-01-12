1958a594876140be3e8ab159f231b76222f3e071
endriwmsi <endriwmsi@gmail.com>
2026-01-12 15:23:05 -0300

    fix: set default values for estadoNascimento and nomePartner fields in capital giro schema

--- src/db/schema/capital-giro.ts
+++ src/db/schema/capital-giro.ts
@@ -17,12 +17,12 @@ export const capitalGiro = pgTable("capital_giro", {
   email: text("email").notNull(),
   estadoCivil: text("estado_civil").notNull(),
   cpf: text("cpf").notNull(),
-  estadoNascimento: text("estado_nascimento").notNull(),
+  estadoNascimento: text("estado_nascimento").notNull().default("tbd"),
   enderecoPessoa: text("endereco_pessoa").notNull(),
   cidadePessoa: text("cidade_pessoa").notNull(),
   estadoPessoa: text("estado_pessoa").notNull(),
 
-  nomePartner: text("nome_partner").notNull(),
+  nomePartner: text("nome_partner").notNull().default("Fabio"),
   documentoUrl: text("documento_url"),
 
   razaoSocial: text("razao_social").notNull(),
